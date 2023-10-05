7d552faecd50f7190c4f8e1358db2cf2008b0d80
Daniel Ryu <rujjiryu99@gmail.com>
2023-10-05 22:18:15 +0900

    apply misc client side experiment changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { MusicEvent } from "./types";
+
+const musicEvents: MusicEvent[] = [
+  {
+    name: "Jazz Night",
+    startDateTime: "2023-11-10T20:00:00Z",
+    endDateTime: "2023-11-10T23:00:00Z",
+    price: 30,
+    externalLink: "http://jazznight.com",
+    artists: ["Artist1", "Artist2"],
+    genre: "Jazz",
+  },
+  {
+    name: "Rock Festival",
+    startDateTime: "2023-12-01T12:00:00Z",
+    endDateTime: "2023-12-01T23:00:00Z",
+    price: 50,
+    externalLink: "http://rockfestival.com",
+    artists: ["RockBand1", "RockBand2", "RockBand3"],
+    genre: "Rock",
+  },
+  {
+    name: "Hip Hop Bash",
+    startDateTime: "2023-12-20T19:00:00Z",
+    price: 40,
+    externalLink: "http://hiphopbash.com",
+    artists: ["HipHopArtist1", "HipHopArtist2"],
+    genre: "Hip Hop",
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>EventGong</h1>
+      <div>
+        <span>Music</span>
+        {/* <span>Esports</span> */}
+      </div>
     </div>
   );
 }
