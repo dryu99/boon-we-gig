b422df09f7fcdb34ed38b3853da6194203511ed4
Daniel Ryu <rujjiryu99@gmail.com>
2023-10-06 20:31:14 +0900

    create a chatgpt specific logger since so we can write usage logs to a separate file (aka im lazy)

--- event-scraper/src/utils/logger.ts
+++ event-scraper/src/utils/logger.ts
@@ -2,28 +2,49 @@ import winston from "winston";
 import path from "path";
 import Config from "./config";
 
-const logger = winston.createLogger({
+const winstonConsoleFormat = winston.format.combine(
+  winston.format.colorize(),
+  winston.format.timestamp(),
+  winston.format.printf((info) => {
+    const { timestamp, level, message, ...args } = info;
+
+    const ts = timestamp.slice(0, 19).replace("T", " ");
+    return `${ts} [${level}]: ${message} ${
+      Object.keys(args).length ? JSON.stringify(args, null, 2) : ""
+    }`;
+  })
+);
+
+const winstonFileFormat = winston.format.combine(
+  winston.format.timestamp(),
+  winston.format.json()
+);
+
+export const logger = winston.createLogger({
   transports: [
     new winston.transports.File({
-      format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.json()
-      ),
+      format: winstonFileFormat,
       filename: path.resolve(__dirname, `../../logs/${Config.NODE_ENV}.log`),
     }),
     new winston.transports.Console({
-      format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.timestamp(),
-        winston.format.printf((info) => {
-          const { timestamp, level, message, ...args } = info;
+      format: winstonConsoleFormat,
+    }),
+  ],
+});
 
-          const ts = timestamp.slice(0, 19).replace("T", " ");
-          return `${ts} [${level}]: ${message} ${
-            Object.keys(args).length ? JSON.stringify(args, null, 2) : ""
-          }`;
-        })
+// we use this logger to aggregate chatgpt usage in a separate file.
+// basically i was too lazy to create my own file writing module lol
+export const chatGptLogger = winston.createLogger({
+  transports: [
+    new winston.transports.File({
+      format: winstonFileFormat,
+      filename: path.resolve(
+        __dirname,
+        `../../logs/chatgpt.${Config.NODE_ENV}.log`
       ),
+    }),
+    new winston.transports.Console({
+      format: winstonConsoleFormat,
     }),
   ],
 });
@@ -44,5 +65,3 @@ export const taggedLogger = (loggerName: string) => {
     },
   };
 };
-
-export default logger;
