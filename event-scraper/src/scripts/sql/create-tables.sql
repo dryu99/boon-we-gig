102e57c1d3cb017e88900f996afe93bbe0c3242a
Daniel Ryu <rujjiryu99@gmail.com>
2023-10-09 21:45:46 +0900

    rename db tables from camelCase -> snake_case

--- event-scraper/src/scripts/sql/create-tables.sql
+++ event-scraper/src/scripts/sql/create-tables.sql
@@ -1,42 +1,42 @@
--- Venue Table
-CREATE TABLE Venue (
+-- venue table
+CREATE TABLE venue (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    instagramId TEXT NOT NULL UNIQUE,
-    reviewStatus TEXT NOT NULL,
+    instagram_id TEXT NOT NULL UNIQUE,
+    review_status TEXT NOT NULL,
     name TEXT,
     address TEXT
 );
 
--- MusicArtist Table
-CREATE TABLE MusicArtist (
+-- music_artist table
+CREATE TABLE music_artist (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name TEXT NOT NULL,
-    reviewStatus TEXT NOT NULL,
+    review_status TEXT NOT NULL,
     genre TEXT,
-    instagramId TEXT UNIQUE,
-    youtubeId TEXT,
-    spotifyId TEXT
+    instagram_id TEXT UNIQUE,
+    youtube_id TEXT,
+    spotify_id TEXT
 );
 
--- MusicEvent Table
-CREATE TABLE MusicEvent (
+-- music_event table
+CREATE TABLE music_event (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     link TEXT NOT NULL,
     country TEXT NOT NULL,
     city TEXT NOT NULL,
-    reviewStatus TEXT NOT NULL,
-    venueId UUID REFERENCES Venue(id) NOT NULL,
-    openDateTime TIMESTAMPTZ,
-    startDateTime TIMESTAMPTZ,
-    earlyPrice DECIMAL(10,2),
-    doorPrice DECIMAL(10,2),
-    eventType TEXT,
-    CHECK(openDateTime <= startDateTime)
+    review_status TEXT NOT NULL,
+    venue_id UUID REFERENCES venue(id) NOT NULL,
+    open_date_time TIMESTAMPTZ,
+    start_date_time TIMESTAMPTZ,
+    early_price DECIMAL(10,2),
+    door_price DECIMAL(10,2),
+    event_type TEXT,
+    CHECK(open_date_time <= start_date_time)
 );
 
--- Junction Table for MusicEvent and MusicArtist many-to-many relationship
-CREATE TABLE MusicEventArtists (
-    PRIMARY KEY(eventId, artistId),
-    eventId UUID REFERENCES MusicEvent(id) NOT NULL,
-    artistId UUID REFERENCES MusicArtist(id) NOT NULL
+-- Junction table for music_event and music_artist many-to-many relationship
+CREATE TABLE music_event_artists (
+    PRIMARY KEY(event_id, artist_id),
+    event_id UUID REFERENCES music_event(id) NOT NULL,
+    artist_id UUID REFERENCES music_artist(id) NOT NULL
 );
