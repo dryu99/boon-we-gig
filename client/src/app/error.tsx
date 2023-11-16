626ba27c6dfd6ce992aec0a397a5e91983ffb6e4
Daniel Ryu <rujjiryu99@gmail.com>
2023-11-16 23:57:51 +0900

    style(client): change error text

--- client/src/app/error.tsx
+++ client/src/app/error.tsx
@@ -14,7 +14,7 @@ export default function Error({
 
   return (
     <main className="flex h-full flex-col items-center justify-center">
-      <h2 className="text-center">Something went wrong x _ x</h2>
+      <h2 className="text-center">Something went wrong ㅠ _ ㅠ</h2>
     </main>
   );
 }
