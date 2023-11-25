0132a1cd76082172ea36fa9ef7b7bf045afa5a46
Daniel Ryu <rujjiryu99@gmail.com>
2023-11-25 19:07:36 +0900

    feat(client): polish concert page styling

--- client/src/app/[locale]/concerts/[id]/page.tsx
+++ client/src/app/[locale]/concerts/[id]/page.tsx
@@ -13,6 +13,7 @@ import {
 import { NewTag, FreeTag, GenreTag } from "@/ui/components/music-event-tags";
 import { InstagramIcon } from "@/ui/svgs/instagram-icon";
 import { LocationIcon } from "@/ui/svgs/location-icon";
+import { ThumbsUpIcon } from "@/ui/svgs/thumbs-up-icon";
 import { getTranslations } from "next-intl/server";
 import { notFound } from "next/navigation";
 
@@ -29,7 +30,6 @@ export default async function ConcertPage({
   const t = await getTranslations("static"); // TODO this is duplicated from the shows page lol
   const genres = extractKeyGenres(musicEvent.artists, params.locale);
   const venue = musicEvent.venue;
-  const externalMapsJson = venue?.externalMapsJson;
   const dateParts = DateHelper.extractParts(
     musicEvent.startDateTime,
     params.locale
@@ -40,51 +40,57 @@ export default async function ConcertPage({
 
   return (
     <div className="flex flex-col">
-      <h2 className="flex flex-col mb-2">
-        <span className="text-3xl">
-          {/* TODO day of week looks too big in korean */}
-          <span
-            className={`mr-2 ${params.locale === "ko" ? "text-2xl" : ""}`}
-          >{`${dateParts.dayOfWeek}`}</span>
-          <span className="mr-2">{`${dateParts.dateStr}`}</span>
-          <span className="mr-2">-</span>
-          <span>{`${dateParts.timeStr}`}</span>
-        </span>
-
-        {venue && (
-          <span>
+      <h2 className="text-3xl mb-1">
+        <span
+          className={`mr-2 ${params.locale === "ko" ? "text-2xl" : ""}`}
+        >{`${dateParts.dayOfWeek}`}</span>
+        <span className="mr-2">{`${dateParts.dateStr}`}</span>
+        <span className="mr-2">-</span>
+        <span>{`${dateParts.timeStr}`}</span>
+      </h2>
+      {venue && (
+        <>
+          <div className="text-center text-xl mb-2">
             <div className="inline-block mr-1" title="Venue">
               <LocationIcon width="16px" />
             </div>
             <Link className="hover:underline" href={`/venues/${venue?.slug}`}>
               {getVenueLocaleName(venue, params.locale)}
             </Link>
-          </span>
-        )}
-      </h2>
-      <div className="flex flex-row justify-center mb-5">
-        {externalMapsJson?.googleMapsUrl && (
-          <div className="mx-1">
-            <GoogleMapsLink url={externalMapsJson?.googleMapsUrl} />
           </div>
-        )}
-        {externalMapsJson?.naverMapsUrl && (
-          <div className="mx-1">
-            <NaverMapsLink url={externalMapsJson?.naverMapsUrl} />
+          <div className="flex flex-row justify-center mb-5">
+            {venue.externalMapsJson?.googleMapsUrl && (
+              <div className="mx-1">
+                <GoogleMapsLink url={venue.externalMapsJson?.googleMapsUrl} />
+              </div>
+            )}
+            {venue.externalMapsJson?.naverMapsUrl && (
+              <div className="mx-1">
+                <NaverMapsLink url={venue.externalMapsJson?.naverMapsUrl} />
+              </div>
+            )}
+            {venue.externalMapsJson?.kakaoMapsUrl && (
+              <div className="mx-1">
+                <KakaoMapsLink url={venue.externalMapsJson?.kakaoMapsUrl} />
+              </div>
+            )}
           </div>
-        )}
-        {externalMapsJson?.kakaoMapsUrl && (
-          <div className="mx-1">
-            <KakaoMapsLink url={externalMapsJson?.kakaoMapsUrl} />
-          </div>
-        )}
-      </div>
+        </>
+      )}
+
       <div className="text-center mb-5">
         <h3 className="font-bold">{t("lineup")}</h3>
         <hr className="w-20 mx-auto mb-1" />
         <div>
           {musicEvent.artists.map((a) => (
-            <div key={a.id}>{a.name}</div>
+            <div key={a.id}>
+              <span className="mr-1">{a.name}</span>
+              {a.isRecommended && (
+                <span className="inline-block" title={t("recommended")}>
+                  <ThumbsUpIcon />
+                </span>
+              )}
+            </div>
           ))}
         </div>
       </div>
