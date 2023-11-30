a129dc8797d7e0d9ee38f220148d38d3ccd615ab
Daniel Ryu <rujjiryu99@gmail.com>
2023-11-30 20:41:13 +0900

    feat(client): set root path as city select page + hide header on this display

--- client/src/app/[locale]/page.tsx
+++ client/src/app/[locale]/page.tsx
@@ -1,15 +1,21 @@
-import { redirect } from "@/lib/navigation";
+import { CITIES } from "@/lib/city";
+import { Link, redirect } from "@/lib/navigation";
 
 export default function IndexPage() {
-  redirect("/seoul");
-  // TODO do this later
-  // return (
-  //   <div>
-  //     <h2>Choose a city</h2>
-  //     <select className="text-black">
-  //       <option>Seoul</option>
-  //       <option>Busan</option>
-  //     </select>
-  //   </div>
-  // );
+  return (
+    <div className="flex flex-col items-center">
+      <h2 className="">choose a city</h2>
+      <div className="flex flex-row">
+        {CITIES.map((city) => (
+          <Link
+            className="p-4 text-xl text-secondary hover:underline cursor-pointer"
+            href={`/${city.toLowerCase()}`}
+            key={city}
+          >
+            {city.toUpperCase()}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 }
